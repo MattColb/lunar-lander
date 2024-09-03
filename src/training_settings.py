6af064e0383c489e406e81a53560796a68ebf80d
Em <colbertmatt12@gmail.com>
2024-09-03 08:26:03 -0500

    Updating test

--- src/training_settings.py
+++ src/training_settings.py
@@ -1,6 +1,5 @@
-
 class Settings:
-    def __init__(self, episodes, epsilon, ep_decay, gamma, lr, buffer_size, batch_size, test_render):
+    def __init__(self, episodes, epsilon, ep_decay, gamma, lr, buffer_size, batch_size, test_render=True, only_test=False):
         self.episodes = episodes
         self.epsilon = epsilon
         self.ep_decay = ep_decay
@@ -9,6 +8,16 @@ class Settings:
         self.buffer_size = buffer_size
         self.batch_size = batch_size
         self.test_render = test_render
+        self.only_test = only_test
 
     def eps_decay(self):
         self.epsilon *= self.ep_decay
+
+    def get_latest_model(self):
+        pass
+
+    def save_settings(self):
+        pass
+
+    def load_settings(self):
+        pass
