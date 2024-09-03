4f67c3cad8bc1fe19cbca7f217871d4e67f3ed82
Em <colbertmatt12@gmail.com>
2024-09-03 08:51:04 -0500

    Removing old checkpoints

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from src.train import train
 from src.test import test
 from src.training_settings import Settings
 
+#Include something for if they want to continue or reset
+
 def main():
     env = gym.make("LunarLander-v2")
     settings = Settings(episodes=1000, epsilon=1, ep_decay=.99, gamma=.99, lr=.001, buffer_size=1000, batch_size=64, test_render=True)
